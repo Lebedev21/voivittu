bd4e985772461086652a86ccf3e7b0c3830fc571
Lebedev21 <admin@vuhuumail.cf>
2021-02-20 22:38:41 +0200

    kek

---
+++ Source Code - Lähdekoodi/ESClicker/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private EsClicks As Integer = 0
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        EsClicks += 1
+        TextBox1.Text = EsClicks
+        If EsClicks > 10000 Then
+            ' No vittu arvaa 2 kertaa... Oikeasti.
+            PictureBox1.Image = My.Resources.KultainenES
+
+            ' EN VITTU TIEDÄ
+            Label2.Visible = True
+            PictureBox2.Visible = True
+
+            ' Soita ES Laulu
+            ESLauluEbin()
+
+        End If
+    End Sub
+
+    Sub ESLauluEbin()
+        My.Computer.Audio.Play(My.Resources.Song,
+        AudioPlayMode.Background)
+    End Sub
+
+
+End Class
